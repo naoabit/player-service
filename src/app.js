942317201fd7a1f335b8e9994ed750155bb12536
Naomi Abitbol <naomi.abitbol@weblib.fr>
2021-10-27 22:36:45 +0200

    Init project commit

---
+++ src/app.js
@@ -0,0 +1,25 @@
+const cors = require('cors');
+const dotenv = require('dotenv');
+const express = require('express');
+const { json, urlencoded } = require('body-parser');
+const routers = require('./route');
+
+dotenv.config();
+
+const loadApp = async () => {
+    await require('./database/model/index.js');
+
+    const app = express();
+    app.use(cors());
+    app.use(json());
+    app.use(urlencoded({ extended: true }));
+    app.use(routers);
+
+    app.listen(process.env.SERVER_PORT, () => {
+        console.log(`App listening on port ${process.env.SERVER_PORT} ...`); // eslint-disable-line
+    });
+
+    return app;
+};
+
+module.exports = loadApp();
---
+++ src/route/player/index.js
@@ -0,0 +1,7 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/v1/players', () => {});
+
+export default router;
