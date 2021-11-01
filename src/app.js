32263e04542a201107e1795b1d3f2d90e093d400
Naomi Abitbol <naomi.abitbol@weblib.fr>
2021-11-01 19:52:44 +0100

    [ADD] Add bodyParser and cors libs

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 const dotenv = require('dotenv');
+const cors = require('cors');
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const routers = require('./route');
 
 dotenv.config();
 
 const loadApp = async () => {
     const app = express();
+    app.use(bodyParser.json({ limit: '50mb' }));
+    app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
+    app.use(cors());
     app.use(routers);
 
     app.listen(process.env.SERVER_PORT, () => {
